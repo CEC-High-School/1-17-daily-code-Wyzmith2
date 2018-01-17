a87785df69fe2eb5a74627c5132808b1966f3d88
Wyzmith <788547@dpsk12.net>
2018-01-17 07:52:15 -0700

    Done

---
+++ Project1/Project1/Source.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdlib.h>
+#include <Windows.h>
+
+using namespace std;
+
+int main() {
+	cout << "Wanna hear a great joke?" << endl;
+	system("pause");
+	system("cls");
+	cout << "What did the chef say about the popcorn?" << endl;
+	Sleep(3500);
+	system("cls");
+	cout << "It tasted CORNY." << endl;
+	system("pause");
+	system("cls");
+	cout << "YOU BETTER LAUGH!!!" << endl;
+	system("pause");
+}
